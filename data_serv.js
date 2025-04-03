33b60f3b69b55600676169174f04ee622eee0627
Yanik Beaulieu <yanikbeaulieu@hotmail.com>
2025-04-02 20:02:10 -0400

    livedata delay and buffer

--- data_serv.js
+++ data_serv.js
@@ -3,20 +3,19 @@ const cors = require('cors')
 require('dotenv').config();
 const mdb = require('./mongooseDB') // mongoose with process.env.MONGO_CLOUD=mongodb+srv://user:password@cluster/collection?retryWrites=true&w=majority&authSource=admin
 
-
-
-const PORT = process.env.PORT || 3003 
-
 const liveDatas = require('./scripts/liveData.js')
 
+const PORT = process.env.PORT || 3003 
 
 
 async function startDatabase() 
 {
-    const url =  process.env.MONGO_CLOUD
-
+    //const url =  process.env.MONGO_CLOUD
+    const dbName = process.env.NODE_ENV === 'production' ? 'datas' : 'devdatas'
+    const mongourl = process.env.MONGO_URL + dbName + process.env.MONGO_OPTIONS  // MONGO_URL=mongodb+srv://yb:zigzag@cluster0-b2xaf.mongodb.net/     //   MONGO_OPTIONS=?retryWrites=true&w=majority&authSource=admin
+    
     try {
-        await mdb.init(url, "datas", async ()=>{    console.log("Collections: ", await mdb.getCollections())   })
+        await mdb.init(mongourl, dbName, async ()=>{    console.log("Collections: ", await mdb.getCollections())   })
        
         const app = express()
         app.set('view engine', 'ejs')
@@ -34,6 +33,8 @@ async function startDatabase()
                 console.log("LiveData  -  v" + liveDatas.version) 
         })
 
+       
+
     } catch (err) {
         console.log('Failed to initialize database:', err)
     }
@@ -41,6 +42,8 @@ async function startDatabase()
 }
 
 startDatabase()
+
+
 
 
 
