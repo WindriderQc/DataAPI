8989145bd86d7c16d2bfa7a135339c153188f3c2
Yanik Beaulieu <yanikbeaulieu@hotmail.com>
2025-10-05 00:40:50 -0400

    naming

---
+++ controllers/databasesController.js
@@ -0,0 +1,49 @@
+const config = require('../config/config');
+const { getDbInfo } = require('../utils/db-utils');
+
+exports.getDatabasesView = async (req, res, next) => {
+    try {
+        const dbs = req.app.locals.dbs;
+        const prodDbName = config.db.modelDbName;
+        const devDbName = 'devDatas'; // As per requirement
+
+        const prodDbInfo = await getDbInfo(dbs[prodDbName]);
+        const devDbInfo = await getDbInfo(dbs[devDbName]);
+
+        res.render('databases', {
+            title: 'Databases',
+            prodDbInfo,
+            devDbInfo,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.copyProdToDev = async (req, res, next) => {
+    try {
+        const dbs = req.app.locals.dbs;
+        const prodDbName = config.db.modelDbName;
+        const devDbName = 'devDatas'; // As per requirement
+        const sourceDb = dbs[prodDbName];
+        const destDb = dbs[devDbName];
+
+        const collections = await sourceDb.listCollections().toArray();
+
+        for (const collection of collections) {
+            const sourceCollection = sourceDb.collection(collection.name);
+            const destCollection = destDb.collection(collection.name);
+
+            await destCollection.deleteMany({});
+            const cursor = sourceCollection.find();
+            const docs = await cursor.toArray();
+            if(docs.length > 0) {
+                await destCollection.insertMany(docs);
+            }
+        }
+        
+        res.status(200).json({ status: 'success', message: 'Production database copied to development successfully.' });
+    } catch (err) {
+        next(err);
+    }
+};
